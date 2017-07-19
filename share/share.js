a5e70906441c724eb1fb935487f86d2fbd539a66
lyndejones1 <lyndejones1@gmail.com>
2017-07-19 13:13:41 -0400

    Share on Twitter

--- share/share.js
+++ share/share.js
@@ -22,13 +22,24 @@ $(function() {
     });
 
     $('.twitter').on('click', function() {
-        var loc = encodeURIComponent('http://bit.ly/somawater'),
-                title = "Beautifully innovative all-natural water filters by Soma — ",
-                w = 580, h = 300,
-                left = (screen.width/2)-(w/2),
-                top = (screen.height/2)-(h/2);
+      var w = 580, h = 300,
+              left = (screen.width/2)-(w/2),
+              top = (screen.height/2)-(h/2);
 
-            window.open('http://hrefshare.com/39e0b' + title + '&url=' + loc, '', 'height=' + h + ', width=' + w + ', top='+top +', left='+ left +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+
+          if ((screen.width < 480) || (screen.height < 480)) {
+              window.open ('http://hrefshare.com/1624e', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+          } else {
+              window.open ('http://hrefshare.com/1624e', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+          }
+
+        // var loc = encodeURIComponent('http://bit.ly/somawater'),
+        //         title = "Beautifully innovative all-natural water filters by Soma — ",
+        //         w = 580, h = 300,
+        //         left = (screen.width/2)-(w/2),
+        //         top = (screen.height/2)-(h/2);
+        //
+        //     window.open('http://hrefshare.com/39e0b' + title + '&url=' + loc, '', 'height=' + h + ', width=' + w + ', top='+top +', left='+ left +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
     });
 
     $('.play').on('click', function() {
