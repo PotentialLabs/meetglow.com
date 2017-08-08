9e72bfa8c6bb72131b0657036e0df0bf178e166c
lyndejones1 <lyndejones1@gmail.com>
2017-08-08 11:41:57 -0400

    fb and twitter new link

--- share/share.js
+++ share/share.js
@@ -17,9 +17,9 @@ $(function() {
 
 
             if ((screen.width < 480) || (screen.height < 480)) {
-                window.open ('http://hrefshare.com/7f14d', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+                window.open ('http://hrefshare.com/d34f4', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
             } else {
-                window.open ('http://hrefshare.com/7f14d', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+                window.open ('http://hrefshare.com/d34f4', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
             }
     });
 
@@ -30,9 +30,9 @@ $(function() {
 
 
           if ((screen.width < 480) || (screen.height < 480)) {
-              window.open ('http://hrefshare.com/b4250', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+              window.open ('http://hrefshare.com/b198f', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
           } else {
-              window.open ('http://hrefshare.com/b4250', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+              window.open ('http://hrefshare.com/b198f', '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
           }
 
         // var loc = encodeURIComponent('http://bit.ly/somawater'),
